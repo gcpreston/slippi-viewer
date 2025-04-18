ca73017c437463ba572ce2f7f377da703c07a293
Graham Preston <graham.preston@gmail.com>
2025-04-18 19:07:42 +0200

    No index.html content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  content: ["./src/**/*.{ts,tsx}"],
   theme: {
     extend: {
       colors: {
